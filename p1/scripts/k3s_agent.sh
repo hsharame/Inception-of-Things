f7ecd7ecbf0cb41b450b835e9c18f1f99bfd9a97
hsharame <hsharame@student.42.fr>
2026-01-16 11:02:15 +0100

    scripts changed

--- p1/scripts/k3s_agent.sh
+++ p1/scripts/k3s_agent.sh
@@ -4,4 +4,8 @@ TOKEN=$(cat /vagrant/confs/node-token)
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
+while ! nc -z ${K3S_SERVER_IP} 6443; do
+	sleep 5
+done
+
 curl -sfL https://get.k3s.io | K3S_URL="https://${K3S_SERVER_IP}:6443" K3S_TOKEN="${TOKEN}" sh -
--- p1/scripts/k3s_server.sh
+++ p1/scripts/k3s_server.sh
@@ -3,7 +3,7 @@
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-curl -sfL https://get.k3s.io | sh -s - --node-ip=${IP_SERVER} --advertise-address=${IP_SERVER} 
+curl -sfL https://get.k3s.io | sh -s - --node-ip=${IP_SERVER} --bind_address=${IP_SERVER} --advertise-address=${IP_SERVER} --write-kubeconfig-mode=644
 
 until sudo test -f /var/lib/rancher/k3s/server/tls/server-ca.crt; do
     sleep 3
@@ -11,6 +11,7 @@ done
 
 mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo sed -i "s/127.0.0.1/${IP_SERVER}/g" /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube
 
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/node-token
