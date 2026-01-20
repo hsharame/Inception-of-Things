e5de59f1e11689f449a2a574476512b90080f99f
hsharame <hsharame@student.42.fr>
2026-01-20 11:27:03 +0100

    another version

--- p1/scripts/k3s_agent.sh
+++ p1/scripts/k3s_agent.sh
@@ -1,18 +1,18 @@
 sudo apt-get update
 sudo apt-get install curl -y
 
-TOKEN=$(cat /vagrant/confs/node-token)
-
 # disable swap
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-while ! nc -z ${K3S_SERVER_IP} 6443; do
-	sleep 5
+until [ -f /vagrant/confs/node-token ]; do
+	sleep 3
+done
+
+TOKEN=$(cat /vagrant/confs/node-token)
+
+until curl -sk https://${K3S_SERVER_IP}:6443/healthz > /dev/null 2>&1; do
+    sleep 3
 done
 
 curl -sfL https://get.k3s.io | K3S_URL="https://${K3S_SERVER_IP}:6443" K3S_TOKEN="${TOKEN}" sh -
-
-curl -sLO https://dl.k8s.io/release/v1.35.0/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
--- p1/scripts/k3s_server.sh
+++ p1/scripts/k3s_server.sh
@@ -5,23 +5,19 @@ sudo apt-get install curl -y
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode=644
+curl -sfL https://get.k3s.io | sh -s - --node-ip="${IP_SERVER}" --tls-san="${IP_SERVER}" --write-kubeconfig-mode=644
 
-until sudo test -f /var/lib/rancher/k3s/server/tls/server-ca.crt; do
+until curl -sk https://127.0.0.1:6443/healthz > /dev/null 2>&1; do
     sleep 3
 done
-
-mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo sed -i "s/127.0.0.1/${IP_SERVER}/g" /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube
-
 
 curl -sLO https://dl.k8s.io/release/v1.35.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-export KUBECONFIG=~/.kube/config
+mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube
 
 mkdir -p /vagrant/confs
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/node-token
