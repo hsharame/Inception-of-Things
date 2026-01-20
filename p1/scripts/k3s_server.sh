a52eea185eca5cf765a768dfdface2bd06ee6472
hsharame <hsharame@student.42.fr>
2026-01-20 18:25:48 +0100

    issue log added

--- p1/scripts/k3s_server.sh
+++ p1/scripts/k3s_server.sh
@@ -17,6 +17,7 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo sed -i "s/127.0.0.1/${IP_SERVER}/g" /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube
 
 mkdir -p /vagrant/confs
