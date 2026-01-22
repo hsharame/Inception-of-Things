3a9c722484c6467877926baa7fcf6d63687cae97
hsharame <hsharame@student.42.fr>
2026-01-22 13:50:09 +0100

    p2: added confs for app2 and app3

--- p2/scripts/k3s_install.sh
+++ p2/scripts/k3s_install.sh
@@ -20,3 +20,6 @@ sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 sudo sed -i "s/127.0.0.1/${IP_SERVER}/g" /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube
 
+kubectl apply -f /vagrant/confs/app1.yaml
+kubectl apply -f /vagrant/confs/app2.yaml
+kubectl apply -f /vagrant/confs/app3.yaml
