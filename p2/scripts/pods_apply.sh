c37ed6490300e63b0e08290103b45562e91bfb57
hanna <hsharame@student.42.fr>
2026-02-25 09:40:05 -0500

    fix(p2): network defined

--- p2/scripts/pods_apply.sh
+++ p2/scripts/pods_apply.sh
@@ -3,7 +3,7 @@ set -e
 echo "127.0.0.1 app1.com" | sudo tee -a /etc/hosts
 echo "127.0.0.1 app2.com" | sudo tee -a /etc/hosts
 
-kubectl apply -f /vagrant/confs/app1.yaml
-kubectl apply -f /vagrant/confs/app2.yaml
-kubectl apply -f /vagrant/confs/app3.yaml
+kubectl apply -f /vagrant/confs/app1/app1.yaml
+kubectl apply -f /vagrant/confs/app2/app2.yaml
+kubectl apply -f /vagrant/confs/app3/app3.yaml
 kubectl apply -f /vagrant/confs/ingress.yaml
